24ed180ab2f63a6b38f8408d257eb726b2b26eee
Viktor Prutyanov <viktor.prutyanov@phystech.edu>
2018-01-06 15:37:47 +0300

    GUI added

--- func-sim/core.py
+++ func-sim/core.py
@@ -13,18 +13,18 @@ class Core:
     def __init__(self, filename, length):
         self.regfile = RegFile()
         self.memory = Memory(filename, length) 
-        self.is_running = False
+        self.is_halted = False
     
     def decode(self, word):
         instrs = [
         #     Name     Mask   Shift CheckVal    Type              Handler
-            ('halt',  0xFFFF,   0,  0x0000, InstrType.ZeroOp,   self.execute_halt),
-            ('nop',   0xFFFF,   0,  0x00A0, InstrType.ZeroOp,   self.execute_nop),
-            ('clr',   0xEFC0,   6,  0o0050, InstrType.OneOp,    self.execute_nop),
-            ('inc',   0xEFC0,   6,  0o0052, InstrType.OneOp,    self.execute_nop),
-            ('dec',   0xEFC0,   6,  0o0053, InstrType.OneOp,    self.execute_nop),
-            ('asr',   0xEFC0,   6,  0o0062, InstrType.OneOp,    self.execute_nop),
-            ('asl',   0xEFC0,   6,  0o0063, InstrType.OneOp,    self.execute_nop),
+            ('halt',  0xFFFF,   0,  0x0000, InstrType.ZeroOp,   self.ex_halt),
+            ('nop',   0xFFFF,   0,  0x00A0, InstrType.ZeroOp,   self.ex_nop),
+            ('clr',   0xEFC0,   6,  0o0050, InstrType.OneOp,    self.ex_nop),
+            ('inc',   0xEFC0,   6,  0o0052, InstrType.OneOp,    self.ex_inc),
+            ('dec',   0xEFC0,   6,  0o0053, InstrType.OneOp,    self.ex_dec),
+            ('asr',   0xEFC0,   6,  0o0062, InstrType.OneOp,    self.ex_asr),
+            ('asl',   0xEFC0,   6,  0o0063, InstrType.OneOp,    self.ex_asl),
             ]
         
         for instr in instrs:
@@ -44,24 +44,42 @@ class Core:
         pc = self.regfile.get_pc()
         self.regfile.set_pc(pc + 1)
 
-    def execute_nop(self):
+    def ex_nop(self, instr):
         self.set_next_pc()
     
-    def execute_halt(self):
-        self.is_running = False
+    def ex_halt(self, instr):
+        self.is_halted = True
+        self.set_next_pc()
+
+    def ex_inc(self, instr):
+        self.regfile.regs[instr['addr']] += 1
+        self.set_next_pc()
+    
+    def ex_dec(self, instr):
+        val = self.regfile.regs[instr['addr']]
+        if val == 0:
+            self.regfile.regs[instr['addr']] = 0xFFFF
+        else:
+            self.regfile.regs[instr['addr']] -= 1
+        self.set_next_pc()
+    
+    def ex_asr(self, instr):
+        self.regfile.regs[instr['addr']] <<= 1
+        self.set_next_pc()
+    
+    def ex_asl(self, instr):
+        self.regfile.regs[instr['addr']] >>= 1
         self.set_next_pc()
 
     def execute(self):
-        self.instr['execute']()
+        self.instr['execute'](self.instr)
+
+    def step(self):
+        pc = self.regfile.get_pc()
+        self.decode(self.memory.read(pc))
+        print(self.instr)
+        self.execute()
 
     def run(self):
-        self.is_running = True
-
-        while True:
-            pc = self.regfile.get_pc()
-            self.decode(self.memory.read(pc))
-            print(self.instr)
-            self.execute()
-            
-            if not self.is_running:
-                return
+        while not self.is_halted:
+            self.step()
--- func-sim/main.py
+++ func-sim/main.py
@@ -3,7 +3,28 @@
 from core import Core
 import sys
 import os
-   
+import tkinter as tk
+
+def update_regs():
+    reg_n = 0
+    for reg in core.regfile.regs:
+        reg_entries[reg_n]['text'] = "0x{0:04X}".format(reg)
+        reg_n = reg_n + 1
+
+def run(event):
+    core.run()
+    update_regs()
+    if core.is_halted:
+        step_button['state'] = 'disabled'
+        run_button['state'] = 'disabled'
+
+def step(event):
+    core.step()
+    update_regs()
+    if core.is_halted:
+        step_button['state'] = 'disabled'
+        run_button['state'] = 'disabled'
+
 if (len(sys.argv) != 2):
     print("usage: {} file.bin".format(sys.argv[0]))
     sys.exit()
@@ -11,4 +32,27 @@ if (len(sys.argv) != 2):
 filename = sys.argv[1]
 
 core = Core(filename, os.path.getsize(filename) // 2)
-core.run()
+
+top = tk.Tk()
+
+step_button = tk.Button(top, text="Step")
+step_button.bind("<Button-1>", step)
+step_button.pack()
+
+run_button = tk.Button(top, text="Run")
+run_button.bind("<Button-1>", run)
+run_button.pack()
+
+reg_labels = []
+reg_entries = []
+reg_n = 0
+for reg in core.regfile.regs:
+    reg_labels.append(tk.Label(top, text="R{}".format(reg_n)))
+    reg_labels[reg_n].pack()
+    reg_entries.append(tk.Label(top, bg='white'))
+    reg_entries[reg_n].pack()
+    reg_n = reg_n + 1
+
+update_regs()
+
+top.mainloop()
